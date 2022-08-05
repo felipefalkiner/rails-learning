5257307ac92d59057967ec86b664be059d69f4e4
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2022-08-05 16:21:07 -0300

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :friends
+  root 'home#index'
+  get 'home/about'
+  # get 'home/index'
+  
+
+
+
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
