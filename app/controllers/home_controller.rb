ef5d9bf4f105cd39940f04feb9694d8a17e794bf
Felipe Magosso Poveda <felipe.magosso@gmail.com>
2022-08-08 17:37:39 -0300

    about controllers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,8 @@ class HomeController < ApplicationController
   end
 
   def about
-
+    # you can write anything you want here
+    # for example an API Call
+    @about_me = "My name is Felipe Falkiner, you can found me on the internet as @felipe_falkiner !"
   end
 end
